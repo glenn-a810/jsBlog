c4fc07be51458e9a245952a98ad90ae0e08174cb
Glenn <a810.glenn@gmail.com>
2022-07-27 21:53:21 +0900

    React state 변경

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import './App.css'
+import { useState } from 'react'
 
 function App() {
+  let post = 'Head Title A'
+  let [title, setTitle] = useState(['Content Title A', 'Content Title B', 'Content Title C'])
+  let [like, setLike] = useState(0)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>Blog</h4>
+      </div>
+      <div className="list">
+        <h4>
+          {title[0]}{' '}
+          <span
+            onClick={() => {
+              setLike(like++)
+            }}
+          >
+            👍
+          </span>
+          {like}
+        </h4>
+        <p>2022.07.27</p>
+      </div>
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>2022.07.27</p>
+      </div>
+      <div className="list">
+        <h4>{title[2]}</h4>
+        <p>2022.07.27</p>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
