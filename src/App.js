5c47fd0302b68dfb7dd5a4627380d53f2a168557
Glenn <a810.glenn@gmail.com>
2022-08-01 22:13:16 +0900

    props

--- src/App.js
+++ src/App.js
@@ -16,8 +16,12 @@ function App() {
 
       {title.map((parm, i) => {
         return (
-          <div className="list">
-            <h4>
+          <div className="list" key={i}>
+            <h4
+              onClick={() => {
+                modal === false ? setModal(true) : setModal(false)
+              }}
+            >
               {title[i]}
               <span
                 onClick={() => {
@@ -34,91 +38,19 @@ function App() {
           </div>
         )
       })}
-      {/*<div className="list">*/}
-      {/*  <button*/}
-      {/*    onClick={() => {*/}
-      {/*      let sortCopy = [...title]*/}
-      {/*      sortCopy.sort()*/}
-      {/*      setTitle(sortCopy)*/}
-      {/*    }}*/}
-      {/*  >*/}
-      {/*    Sort*/}
-      {/*  </button>*/}
-      {/*  <h4>*/}
-      {/*    {title[0]}{' '}*/}
-      {/*    <span*/}
-      {/*      onClick={() => {*/}
-      {/*        // setLike(like++)*/}
-      {/*        let likeCopy = [...like]*/}
-      {/*        likeCopy[0] += 1*/}
-      {/*        setLike(likeCopy)*/}
-      {/*      }}*/}
-      {/*    >*/}
-      {/*      👍*/}
-      {/*    </span>*/}
-      {/*    {like[0]}*/}
-      {/*  </h4>*/}
-      {/*  <button*/}
-      {/*    onClick={() => {*/}
-      {/*      let copy = [...title]*/}
-      {/*      copy[0] = 'Content Title A changed'*/}
-      {/*      setTitle(copy)*/}
-      {/*    }}*/}
-      {/*  >*/}
-      {/*    Change*/}
-      {/*  </button>*/}
-      {/*  <p>2022.07.27</p>*/}
-      {/*</div>*/}
-      {/*<div className="list">*/}
-      {/*  <h4>*/}
-      {/*    {title[1]}{' '}*/}
-      {/*    <span*/}
-      {/*      onClick={() => {*/}
-      {/*        // setLike(like++)*/}
-      {/*        let likeCopy = [...like]*/}
-      {/*        likeCopy[1] += 1*/}
-      {/*        setLike(likeCopy)*/}
-      {/*      }}*/}
-      {/*    >*/}
-      {/*      👍*/}
-      {/*    </span>*/}
-      {/*    {like[1]}*/}
-      {/*  </h4>*/}
-      {/*  <p>2022.07.27</p>*/}
-      {/*</div>*/}
-      {/*<div className="list">*/}
-      {/*  <h4*/}
-      {/*    onClick={() => {*/}
-      {/*      modal === true ? setModal(false) : setModal(true)*/}
-      {/*    }}*/}
-      {/*  >*/}
-      {/*    {title[2]}{' '}*/}
-      {/*    <span*/}
-      {/*      onClick={() => {*/}
-      {/*        // setLike(like++)*/}
-      {/*        let likeCopy = [...like]*/}
-      {/*        likeCopy[2] += 1*/}
-      {/*        setLike(likeCopy)*/}
-      {/*      }}*/}
-      {/*    >*/}
-      {/*      👍*/}
-      {/*    </span>*/}
-      {/*    {like[2]}*/}
-      {/*  </h4>*/}
-      {/*  <p>2022.07.27</p>*/}
-      {/*</div>*/}
 
-      {modal === true ? <Modal /> : null}
+      {modal === true ? <Modal color={'skyblue'} title={title} /> : null}
     </div>
   )
 }
 
-const Modal = () => {
+const Modal = (props) => {
   return (
-    <div className="modal">
-      <h4>Modal Title</h4>
+    <div className="modal" style={{ background: props.color }}>
+      <h4>{props.title}</h4>
       <p>Modal Date</p>
       <p>Modal Contents</p>
+      <button>Edit</button>
     </div>
   )
 }
